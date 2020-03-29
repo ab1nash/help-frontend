587a68e3e8b25f01e84eb9829b2769ab96c24675
Ishaan Shah <ishaan.n.shah@gmail.com>
2020-03-29 19:50:18 +0530

    Add searchbox

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
 import { view } from 'react-easy-state';
-import { GoogleMap, LoadScript, Marker, useGoogleMap } from '@react-google-maps/api'
+import React, { useState } from 'react';
+import { GoogleMap, LoadScript, Marker, useGoogleMap, StandaloneSearchBox } from '@react-google-maps/api'
+import Form from 'react-bootstrap/Form';
 
 import { MapStore, MapActions } from "../stores/map";
 
 
-const libraries = ["drawing"];
+const libraries = ["drawing", "places"];
 
 export const Map = view(() => {
+    const [searchBox, setSearchBox] = useState(undefined);
 
     const googleMapsAPIKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
     // const map: GoogleMap = useGoogleMap();
@@ -22,12 +24,40 @@ export const Map = view(() => {
         MapActions.setMarkerPosition(e.latLng.lat(), e.latLng.lng());
     };
 
+    const onLoad = (ref: any) => setSearchBox(ref);
+
+    const onPlacesChanged = () => {
+        // @ts-ignore
+        let loc = searchBox.getPlaces()[0].geometry.location;
+        let lat = loc.lat(), lng = loc.lng();
+        MapActions.setMarkerPosition(lat, lng);
+    };
+
     return (
         <LoadScript googleMapsApiKey={googleMapsAPIKey} libraries={libraries}>
-            <GoogleMap mapContainerStyle={{maxWidth: "100%", minHeight: "100vh",}} zoom={15} center={{ lat, lng }}
-                       onCenterChanged={onCenterChanged}>
+            <GoogleMap mapContainerStyle={{maxWidth: "100%", minHeight: "100vh",}} zoom={15} center={{ lat, lng }} onCenterChanged={onCenterChanged}>
+                <StandaloneSearchBox
+                    onLoad={onLoad}
+                    onPlacesChanged={onPlacesChanged}
+                >
+                  <Form.Control
+                    type="text"
+                    placeholder="Search"
+                    style={{
+                      width: `360px`,
+                      height: `50px`,
+                      padding: `0 12px`,
+                      textOverflow: `ellipses`,
+                      position: "absolute",
+                      left: "50%",
+                      top: "10px",
+                      marginLeft: "-180px"
+                    }}
+                  />
+                </StandaloneSearchBox>
                 <Marker position={{lat, lng}} draggable={true} onDragEnd={onDragEnd} />
             </GoogleMap>
         </LoadScript>
     )
 });
+
